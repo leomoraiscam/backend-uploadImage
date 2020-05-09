f83111ad0ef914c6e559c3e820a869d568ebb690
lmorais <leozonnn@gmail.com>
2020-05-09 18:05:45 -0500

    configurando o servidor para utilizar a estrutura de dados json e utilizaçao do morgan

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const morgan = require('morgan');
 const routes = require('./routes');
 
 const app = express();
 
 app.use(routes);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
 
 app.listen(3333, () => {
   console.log('Server is running');
