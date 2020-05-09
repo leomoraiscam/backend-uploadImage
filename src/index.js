6cb77eded26abce533029bf849d2ebb632c66477
lmorais <leozonnn@gmail.com>
2020-05-09 18:01:03 -0500

    Criando um arquivo externo de rotas, configurando o servidor para utilizar as rotas e recursos definidos

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 const express = require('express');
+const routes = require('./routes');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Pagina principal');
-});
+app.use(routes);
 
 app.listen(3333, () => {
   console.log('Server is running');
