a932aee40649b36417eb2957a6cee71f25231621
lmorais <leozonnn@gmail.com>
2020-05-09 14:29:44 -0500

    Estruturando a pasta da aplicação e definindo as primeiras regas de lint

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  return res.send("Pagina principal");
+});
+
+app.listen(3333, () => {
+  console.log("Server is running");
+});
